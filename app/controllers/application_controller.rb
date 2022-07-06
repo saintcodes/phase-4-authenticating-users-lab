0d682e05ef8101962a9e9cfd516d4aeabdb3e21d
Michelle <clairsin@gmail.com>
2022-07-06 16:41:24 -0400

    commit tree 5a8db2fda1a55e8efb7c9352dcff32d74d24789e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+private
+  def render_not_found
+    render json: {error: "Record not found"}, status: :not_found
+  end
 
 end
